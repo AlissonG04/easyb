775f75d416052315b51d1ef0135062d49adf6b0c
Alisson G. <eng.alissongabriel@gmail.com>
2025-05-19 13:03:03 -0300

    mudança na logo e nome do sistema

--- src/components/Watermark.jsx
+++ src/components/Watermark.jsx
@@ -1,7 +1,10 @@
 export default function Watermark() {
   return (
-    <div className="absolute bottom-4 right-4 text-sm font-bold text-blue-800">
-      EASY <span className="text-blue-500">BALANCE</span>
+    <div
+      className="absolute bottom-4 right-4 text-sm font-bold"
+      style={{ color: "#1d2b52" }}
+    >
+      SYN SCALE
     </div>
   );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,14 +5,17 @@ export default function LoginPage() {
   return (
     <div className="w-screen h-screen bg-gray-300 flex flex-col justify-center items-center relative">
       <div className="bg-white p-10 rounded shadow-md w-full max-w-sm text-center">
-        <img src={logo} alt="Logo Easy Balance" className="mx-auto mb-4 w-20" />
-        <h1 className="text-xl font-bold text-blue-800">
-          EASY <span className="text-blue-500">BALANCE</span>
+        <img src={logo} alt="Logo SYNSCALE" className="mx-auto mb-4 w-20" />
+        <h1 className="text-xl font-bold" style={{ color: "#1d2b52" }}>
+          SYN SCALE
         </h1>
         <LoginForm />
       </div>
-      <span className="absolute bottom-4 right-4 text-sm font-bold text-blue-800">
-        EASY <span className="text-blue-500">BALANCE</span>
+      <span
+        className="absolute bottom-4 right-4 text-sm font-bold"
+        style={{ color: "#1d2b52" }}
+      >
+        SYN SCALE
       </span>
     </div>
   );
